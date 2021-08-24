fccb893a630f5d60d324231220dcc3e5c380129e
Sahil Gulati <64752326+sahil150395@users.noreply.github.com>
2021-08-24 05:59:24 +0000

    day 8

--- main.py
+++ main.py
@@ -3,4 +3,22 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
+# def greet():
+#     print("1")
+#     print("2")
+#     print("3")
 
+# greet()
+
+#Function that allows input
+# def greet_with_name(name):
+#     print(f"Hello {name}!")
+
+# greet_with_name("sahil")
+
+#Function with more than one input
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"Welcome to {location}")
+
+greet_with(name="Sahil", location="Amritsar")
